4abccf9747caf48c3e40974c1793a777ecd4ec7b
Rafael T <rt.lucena@outlook.com>
2024-05-20 19:06:21 -0300

    Alerta de Boas-vindas (index.html)

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+//Alerta de Boas-vindas (index.html)
+window.onload = function() {
+    if (window.location.pathname == "/" || window.location.pathname == "/index.html") {
+        alert("Seja bem-vindo!");
+    }
+}
