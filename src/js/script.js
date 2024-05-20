752e708cb7b7f1017f6dbe75b9c04020bf75b67b
Rafael T <rt.lucena@outlook.com>
2024-05-20 19:07:44 -0300

    Função que muda a cor do site

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,12 @@ window.onload = function() {
         alert("Seja bem-vindo!");
     }
 }
+
+//Função que muda a cor do site
+var cores = ['slategray', 'khaki', 'olivedrab', 'white'];
+var corAtual = 0;
+
+function mudarCor() {
+    document.body.style.backgroundColor = cores[corAtual % cores.length];
+    corAtual++;
+}
